41eef49f171b7e5e70d6d167842d037f5ea4704c
Sucheta <152525316+AstroSucheta@users.noreply.github.com>
2024-10-15 16:36:13 +0530

    Update exampledag.py

--- astronomer-example/dags/exampledag.py
+++ astronomer-example/dags/exampledag.py
@@ -27,6 +27,7 @@ import requests
 
 
 # Define the basic parameters of the DAG, like schedule and start_date
+#test commit for rootfolder deploy
 @dag(
     start_date=datetime(2024, 1, 1),
     schedule="@daily",
